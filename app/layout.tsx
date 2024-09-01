faff6f3f68ef3250d31a926d1d253ab3a067a28b
Prantor-Hawlader <prantor502@gmail.comm>
2024-09-01 23:32:16 +0600

    Home page title added

---
+++ app/layout.tsx
@@ -0,0 +1,51 @@
+import "@/styles/globals.css";
+import { Metadata, Viewport } from "next";
+import clsx from "clsx";
+
+import { Providers } from "./providers";
+
+import { fontMono } from "@/config/fonts";
+
+export const metadata: Metadata = {
+  title: {
+    default: "Fake User Generator",
+    template: `%s - Fake User Generator`,
+  },
+  description: "Fake User Generator Website",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html suppressHydrationWarning lang="en">
+      <head />
+      <body
+        className={clsx(
+          "min-h-screen bg-background font-sans antialiased",
+          fontMono.variable
+        )}
+      >
+        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+          <div className="relative flex flex-col h-screen">
+            <main className="container mx-auto max-w-7xl pt-4 px-6 flex-grow">
+              {children}
+            </main>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { title } from "@/components/primitives";
+
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-1 md:py-2">
+      <div className="inline-block max-w-xl text-center justify-center">
+        <h1 className={title()}>Fake&nbsp;</h1>
+        <h1 className={title({ color: "violet" })}>User&nbsp;</h1>
+        <h1 className={title()}>Generator</h1>
+      </div>
+    </section>
+  );
+}
